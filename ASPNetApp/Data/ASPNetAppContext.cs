84f6be2cce2e2e9215eca7200a458fa3262d3a09
Rémi <remarcoux@hotmail.fr>
2019-03-26 15:56:18 +0100

    oui2

---
+++ ASPNetApp/Data/ASPNetAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNetApp.Models
+{
+    public class ASPNetAppContext : DbContext
+    {
+        public ASPNetAppContext (DbContextOptions<ASPNetAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPNetApp.Models.Movie> Movie { get; set; }
+    }
+}
--- ASPNetApp/Startup.cs
+++ ASPNetApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ASPNetApp.Models;
 
 namespace ASPNetApp
 {
@@ -33,6 +35,9 @@ namespace ASPNetApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ASPNetAppContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ASPNetAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
