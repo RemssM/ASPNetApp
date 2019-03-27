3871ae40512df04db83f277c322ed6758b49ab00
Rémi <remarcoux@hotmail.fr>
2019-03-27 17:09:21 +0100

    Fin de journée relou le login

---
+++ ASPNetApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetApp.Models
+{
+    public class User : IdentityUser
+    {
+        public string nom { get; set; }
+        public string prenom { get; set; }
+    }
+}
